3cd2e14eca70cd5893686b5ea3b4f11ac665666c
franciscocaballero <caballerofrancisco22@yahoo.com>
2021-06-30 20:58:00 -0400

    making api requests

--- script.js
+++ script.js
@@ -6,11 +6,36 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 //old school way of doing AJAX in Javscript:
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.eu/rest/v2/name/spain');
-request.send();
 
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
-});
+const getCoutrydata = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+    <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+      </article>
+      `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCoutrydata('Spain');
+getCoutrydata('Japan');
+getCoutrydata('usa');
+getCoutrydata('china');
